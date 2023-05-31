52688a2eb82f5e651b8cddb3b29cf9011ae97eb3
Diegojadrian97 <diegojadrian97@gmail.com>
2023-05-31 12:56:12 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavBarImport from "./Components/Navbar/Navbar.jsx";
+import './assets/styles.css';
+
+
+
+class App extends React.Component {
+  render(){
+    return (    
+    <div>
+    <NavBarImport
+    />
+     </div>
+  )
+}
+}
+
+export default App
+
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+import './Navbar.css'
+import CartIcon from '../cart/CartWidget.jsx'
+
+const NavBarImport = () => {
+  return (
+    
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container fluid>
+          <Navbar.Brand href="#home">Diego</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className='NavItem' href="#features">Productos</Nav.Link>
+              <Nav.Link className='NavItem' href="#pricing">Ofertas</Nav.Link>
+              
+          </Nav>
+          <Nav>
+          
+            <NavDropdown title="Contacto" align='end' id="collasible-nav-dropdown">
+                <NavDropdown.Item bg="dark" href="#action/3.1">Contactanos</NavDropdown.Item>
+                <NavDropdown.Item bg="dark" href="#action/3.2">
+                  Soporte Técnico
+                </NavDropdown.Item>
+              </NavDropdown>
+
+            <CartIcon
+            
+            />
+
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBarImport
